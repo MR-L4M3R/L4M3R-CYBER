3cb2c2d9bbbf025de38d3bf16695e4cb4e14857c
MR.L4M3R <83567167+MR-L4M3R@users.noreply.github.com>
2021-05-26 19:02:40 +0700

    Add files via upload

---
+++ M3R.sh
@@ -0,0 +1,131 @@
+clear
+figlet HACKER |lolcat
+echo "\33[31;1m[\33[0;32m======================================\33[31;1m]"
+echo "\33[31;1m[          \33[0;36mAuthor : MR.L4M3R          \33[31;1m]"
+echo "\33[31;1m[      \33[0;36m[!] Gunakan Dengan Bijak Tod [!]    \33[31;1m]"
+echo "\33[31;1m[        \33[0;36m[✓] SUMBER CYBER TEAM [✓]        \33[31;1m]"
+echo "\33[31;1m[\33[0;32m======================================\33[31;1m]"
+echo
+echo
+echo "\33[1;33m[$] Silakan Pilih TOOL Anda [$]"
+echo "\33[0;32m[1] \33[31;1mSpam SMS"
+echo "\33[0;32m[2] \33[31;1mSpam Call"
+echo "\33[0;32m[3] \33[31;1mDDOS Attack"
+echo "\33[0;32m[4] \33[31;1mLink Web Vuln Deface Webdav"
+echo "\33[0;32m[5] \33[31;1mMembuat Script Deface Tanpa Coding"
+echo "\33[0;32m[6] \33[31;1mHack Facebook Terbaru 2019"
+echo "\33[0;32m[7] \33[31;1mCara Memutar Lagu/Video YuTube Di Termux"
+echo "\33[0;32m[8] \33[31;1mScript Termux TERBARU"
+echo "\33[0;32m[9] \33[31;1mHack CCTV"
+echo "\33[0;32m[10] \33[31;1mVirtex WhatsApp"
+echo "\33[0;32m[11] \33[31;1mLacak IP Websites"
+echo "\33[0;32m[12] \33[31;1mNonton Film"
+echo "\33[0;32m[13] \33[31;1mBermain FB,YT,Google"
+echo "\33[0;32m[14] \33[31;1mSedang Proses, Mohon Sabar"
+echo "\33[0;32m[15] \33[31;1mSedang Proses, Mohon Sabar"
+echo
+echo "\33[0;32m[99] \33[31;1mInstall Bahan"
+echo "\33[0;32m[88] \33[31;1mKELUAR"
+echo "Install Bahan Dulu Supaya Gak Gagal"
+echo
+read -p "Masukan Pilihan Anda :" roniyt
+
+case $roniyt in
+1)
+clear
+sh spamroniyt.sh
+;;
+2)
+clear
+php spamcall.php
+;;
+3)
+clear
+python2 attack.py
+;;
+4)
+clear
+sh vulnweb.sh
+;;
+5)
+clear
+python2 creator.py
+;;
+6)
+clear
+python2 Dark.py
+;;
+7)
+clear
+apt update
+apt upgrade
+apt install python
+apt install mpv
+pip install mps-youtube
+pip install youtube-dl
+mpsyt
+;;
+8)
+clear
+apt update
+apt upgrade
+git clone https://github.com/termux/termux-api-package
+cd termux-api-package
+scripts
+;;
+9)
+clear
+git clone http://github.com/kancotdiq/ipcs
+cd ipcs
+python2 scan.py
+;;
+10)
+clear
+git clone https://github.com/muhammadfathul/VIRTEX
+cd VIRTEX
+sh virtex.sh
+;;
+11)
+apt update
+apt upgrade
+pkg install nmap
+clear
+figlet ZK1IP | lolcat
+echo "Author : ZK1_CYBER" | lolcat
+echo "[+] Perintah Ketik : nmap -v -A (link website) [+]" | lolcat
+echo "[!] Warning !! Setiap Detik IP Bisa Berubah - Ubah" | lolcat
+;;
+12)
+pkg install fish
+echo "Ketik : telnet towel.blinkenlights.nl" | lolcat
+fish
+telnet towel.blinkenlights.nl
+;;
+13)
+pkg install w3m
+echo "[=] Perintah Ketik : w3m Facebook.com [=]" | lolcat
+;;
+99)
+clear
+pkg update
+pkg upgrade
+pkg install figlet
+pkg install php
+pkg install toilet
+pip2 install lolcat
+pkg install git
+pkg install python
+pkg install python2
+sh M3R.sh
+;;
+88)
+clear
+echo "Berhasil Keluar"
+echo "Terimakasih Telah Memakai TOOL Saya"
+;;
+*)
+echo "Maaf Nomor Yang Anda Ketik Salah"
+sleep 5
+sh ZK1.sh
+;;
+esac
